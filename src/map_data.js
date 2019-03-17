68de42b9f98034845f703a413f14cc3340b211a3
Left Burner <lefttheburneron@gmail.com>
2019-03-17 15:11:38 -0400

    Update chapter data.

--- src/map_data.js
+++ src/map_data.js
@@ -1,185 +1,367 @@
 export default [
   {
-    location: "Atlanta",
+    location: "GWU Atlanta",
     lat: 33.748995,
     lng: -84.387982,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Atlanta, GA, USA chapter of Game Workers Unite.',
+      applicationLink: '',
+      twitter: 'GWU_Atlanta',
+      email: 'GWUAtlanta@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Austin",
+    location: "GWU Austin",
     lat: 30.267153,
     lng: -97.743061,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Austin, TX, USA chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLSe6NQFsIcWVVBdA2vvMJbs5tnLNCEIsw6Yyin9b4p3zYqNVjQ/viewform',
+      twitter: 'GWU_Austin',
+      email: 'GWUAustinLocal@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Australia",
+    location: "GWU Australia",
     lat: -25.274398,
     lng: 133.775136,
     country: "Australia",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The national Australia chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLSdKRJv8DKsOT92V4r2mzWHVaYfwHrG405hea8UV9tZTgW36Ig/viewform',
+      twitter: 'GWU_Aus',
+      email: 'GWUAustralia@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Baltimore",
+    location: "GWU Baltimore",
     lat: 39.290385,
     lng: -76.612189,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Baltimore, MD, USA chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLScAv2ykDUnBzYZ8N5vtw_lAcv7HargbqIkjKvdCIL7Ydqvx6g/viewform',
+      twitter: 'GWU_Baltimore',
+      email: 'GWUBaltimore@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Bay Area",
+    location: "GWU Bay Area",
     lat: 37.827178,
     lng: -122.291308,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Bay Area, CA, USA chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLSdVe5gWVEqXoh2Y3tdJiDvBz4cvNWgKjBZ_MUYjoFYuukuldw/viewform',
+      twitter: 'GWU_BayArea',
+      email: 'GWUBayArea@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Boston",
+    location: "GWU Boston",
     lat: 42.360083,
     lng: -71.05888,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The Boston, MA, USA chapter of Game Workers Unite.',
+      applicationLink: 'https://goo.gl/forms/U2Xy1uDtfvw73BgA3',
+      twitter: 'GWU_Boston',
+      email: 'GWUBoston@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Brasil",
+    location: "GWU Brasil",
     lat: -14.235004,
     lng: -51.92528,
     country: "Brazil",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The national Brasil chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLSc5Q7r3_yr5fJL3IW5fzh5PwYe4L2K55aP3-rkngZBQn_5fXQ/viewform',
+      twitter: 'GWU_Brasil',
+      email: 'GWUBrasil@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Chicago",
+    location: "GWU Chicago",
     lat: 41.878114,
     lng: -87.629798,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Chicago, IL, USA chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLScJJF2zi6DRDl1f4FELY0GNLx7_8NFt_W-j9QfSaOO30EDf9w/viewform',
+      twitter: 'GWU_Chicago',
+      email: 'GWUChicago@gmail.com',
+      website: 'GWU-Chicago.org'
+    }
   },
   {
-    location: "DC",
+    location: "GWU DC",
     lat: 38.907192,
     lng: -77.036871,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Washington D.C. chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/1JiVog4u6VA-WoS4JdtGtPfs0qFHBYaSWapEIF7DK5y8/edit',
+      twitter: 'GWU_DC',
+      email: 'GWUDCLocal@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Dallas",
+    location: "GWU Dallas",
     lat: 32.776664,
     lng: -96.796988,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Dallas, TX, USA chapter of Game Workers Unite.',
+      applicationLink: 'https://goo.gl/forms/RBFvHJZrjtduTfmq2',
+      twitter: 'GWU_Dallas',
+      email: 'GWUDallas@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Detroit",
+    location: "GWU Detroit",
     lat: 42.331427,
     lng: -83.045754,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Detroit, MI, USA chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/1HeaQMUpokdK4Em9bdYDc13emz-sLRV4LWy8F8EdSyGU/edit',
+      twitter: 'GWU_Detroit',
+      email: 'GWUDetroit@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Deutschland",
+    location: "GWU Deutschland",
     lat: 51.165691,
     lng: 10.451526,
     country: "Germany",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The national Deutschland chapter of Game Workers Unite.',
+      applicationLink: 'https://goo.gl/forms/53CgNOT2jrSVwoHq1',
+      twitter: 'GWU_Deutschland',
+      email: 'GWUDeutschland@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "UK",
+    location: "GWU UK",
     lat: 53.414323,
     lng: -2.294885,
     country: "United Kingdom",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'National Game Workers Unite union of the United Kingdom. A branch of [IWGB](https://iwgb.org.uk/).',
+      applicationLink: 'https://iwgb.org.uk/join/gwu',
+      twitter: 'GWU_UK',
+      email: 'contact@gwu-uk.org',
+      website: 'gwu-uk.org'
+    }
   },
   {
-    location: "Los Angeles",
+    location: "GWU Los Angeles",
     lat: 34.052234,
     lng: -118.243685,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Los Angeles, CA, USA chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLScggE2ibjn2t9_1VcNT2r4yitlasP_XMsEJu_pmZ99zDZbx1w/viewform',
+      twitter: 'GWU_LosAngeles',
+      email: 'GWULosAngeles@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Montréal",
+    location: "GWU Montréal",
     lat: 45.501689,
     lng: -73.567256,
     country: "Canada",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Montréal, Canada chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLSc59I5tMp8fJQJMUDsWoYfdCvLN95nLVIkOg5mKJlaa98vsOw/viewform',
+      twitter: 'GWU_Montreal',
+      email: 'GWUMontreal@gmail.com',
+      website: 'GWUMTL.com'
+    }
   },
   {
-    location: "New York City",
+    location: "GWU New York City",
     lat: 40.712775,
     lng: -74.005973,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local NYC, NY, USA chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/1dAqrFD9b9chl5AYWWScboCQUe08Z8_0nR7JzsgUYZFM/edit',
+      twitter: 'GWU_NewYorkCity',
+      email: 'GWUNewYorkCity@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Orange County",
+    location: "GWU Orange County",
     lat: 33.717471,
     lng: -117.831143,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Orange Country, CA, USA chapter of Game Workers Unite.',
+      applicationLink: '',
+      twitter: 'GWUOrangeCounty',
+      email: 'GWUOrangeCounty@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Orlando",
+    location: "GWU Orlando",
     lat: 28.538336,
     lng: -81.379236,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Orlando, FL, USA chapter of Game Workers Unite.',
+      applicationLink: 'https://goo.gl/forms/ElyN68EiHMLnkovs1',
+      twitter: 'GWU_Orlando',
+      email: 'GWUOrlando@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Ottawa",
+    location: "GWU Ottawa",
     lat: 45.42153,
     lng: -75.697193,
     country: "Canada",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Ottawa, Canada chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/1uQmzlz7zpJrSExXNumjgi_iadVFC7THcWyxwSQdDF8Q/',
+      twitter: 'GWU_Ottawa',
+      email: 'GWUOttawa@gmail.com',
+      website: ''
+    }
   },
   {
     location: "STJV",
     lat: 46.227638,
     lng: 2.213749,
     country: "France",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The video game workers\' union in France.',
+      applicationLink: 'https://www.stjv.fr/en/membership/',
+      twitter: 'STVJ_fr',
+      email: 'Contact@STJV.fr',
+      website: 'STVJ.fr/en'
+    }
   },
   {
-    location: "Seattle",
+    location: "GWU Seattle",
     lat: 47.60621,
     lng: -122.332071,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Seattle, WA, USA chapter of Game Workers Unite.',
+      applicationLink: 'https://goo.gl/forms/QJiBTURiU0wFNxD93',
+      twitter: 'GWU_Seattle',
+      email: 'GWUSeattle@gmail.com',
+      website: 'gameworkersseattle.wordpress.com'
+    }
   },
   {
-    location: "Spain",
+    location: "GWU Spain",
     lat: 40.463667,
     lng: -3.74922,
     country: "Spain",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The national España chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLSdP1wlXsu39sfvoBAgvaLO3nLSEAhFXERnJHqK7dl3ok6GMrA/viewform',
+      twitter: 'GWU_Spain',
+      email: 'contact@gwuspain.org',
+      website: 'gwuspain.org'
+    }
   },
   {
-    location: "Sweden",
+    location: "GWU Sweden",
     lat: 61.889081,
     lng: 15.457691,
     country: "Sweden",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The national Sweden chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLSccI5tefFOo9cr5xAboUr_Hr4Wdk3gM4o__MrNTekXe7Ey3ig/viewform?usp=sf_link',
+      twitter: 'GWU_Sweden',
+      email: 'info@gwu-sweden.org',
+      website: 'gwu-sweden.org'
+    }
   },
   {
-    location: "Toronto",
+    location: "GWU Toronto",
     lat: 43.653226,
     lng: -79.383184,
     country: "Canada",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Toronto, ON, Canada chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLSfLTkYiir3BU2DFgJm8PXvSBmJCxoslso-iKEhgnYTWb9MEmg/viewform',
+      twitter: 'GWU_Toronto',
+      email: 'GWUToronto@gmail.com',
+      website: ''
+    }
   },
   {
-    location: "Triangle",
+    location: "GWU Triangle",
     lat: 35.899168,
     lng: -78.86364,
     country: "USA",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Triangle Region, NC, USA chapter of Game Workers Unite.',
+      applicationLink: 'https://goo.gl/forms/1FUQONVZ092tMnx23',
+      twitter: 'GWU_Triangle',
+      email: 'GWUTriangle@gmail.com',
+      website: 'https://goo.gl/forms/1FUQONVZ092tMnx23'
+    }
   },
   {
-    location: "Vancouver",
+    location: "GWU Vancouver",
     lat: 49.282729,
     lng: -123.120738,
     country: "Canada",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local Vancouver, Canada chapter of Game Workers Unite.',
+      applicationLink: 'https://docs.google.com/forms/d/e/1FAIpQLSdCixTyvEsGcr3FNjK-1ZcNmm7O54Tw0-lOo9s_lC5m00-6bQ/viewform',
+      twitter: 'GWU_Vancouver',
+      email: 'GWUVancouver@gmail.com',
+      website: ''
+    }
   },
   {
     location: "Italy",
@@ -196,11 +378,18 @@ export default [
     chapter: false
   },
   {
-    location: "Russia",
+    location: "GWU Russia",
     lat: 55.755826,
     lng: 37.6173,
     country: "Russia",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local national Russia chapter of Game Workers Unite.',
+      applicationLink: '',
+      twitter: '',
+      email: '',
+      website: ''
+    }
   },
   {
     location: "Taipei",
@@ -259,18 +448,32 @@ export default [
     chapter: false
   },
   {
-    location: "Poland",
+    location: "GWU Poland",
     lat: 51.919438,
     lng: 19.145136,
     country: "Poland",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local national Poland chapter of Game Workers Unite.',
+      applicationLink: '',
+      twitter: '',
+      email: '',
+      website: ''
+    }
   },
   {
-    location: "Ukraine",
+    location: "GWU Ukraine",
     lat: 48.379433,
     lng: 31.16558,
     country: "Ukraine",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local national Ukraine chapter of Game Workers Unite.',
+      applicationLink: '',
+      twitter: '',
+      email: '',
+      website: ''
+    }
   },
   {
     location: "Las Vegas",
@@ -280,11 +483,18 @@ export default [
     chapter: false
   },
   {
-    location: "Ireland",
+    location: "GWU Ireland",
     lat: 53.142367,
     lng: -7.692054,
     country: "Ireland",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local national Ireland chapter of Game Workers Unite.',
+      applicationLink: '',
+      twitter: '',
+      email: '',
+      website: ''
+    }
   },
   {
     location: "Alberta",
@@ -322,11 +532,18 @@ export default [
     chapter: false
   },
   {
-    location: "Malaysia",
+    location: "GWU Malaysia",
     lat: 4.210484,
     lng: 101.975766,
     country: "Malaysia",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local national Malaysia chapter of Game Workers Unite.',
+      applicationLink: '',
+      twitter: '',
+      email: '',
+      website: ''
+    }
   },
   {
     location: "Vietnam",
@@ -357,11 +574,18 @@ export default [
     chapter: false
   },
   {
-    location: "Argentina",
+    location: "GWU Argentina",
     lat: -38.416097,
     lng: -63.616672,
     country: "Argentina",
-    chapter: true
+    chapter: true,
+    chapterInfo: {
+      description: 'The local national Argentina chapter of Game Workers Unite.',
+      applicationLink: '',
+      twitter: '',
+      email: '',
+      website: ''
+    }
   },
   {
     location: "New Zealand",
