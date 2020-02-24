0f0c53b77c5cddbb4176412d947536902ced4c17
Left Burner <lefttheburneron@gmail.com>
2020-02-24 03:38:29 -0500

    Fix externals include for built demo page.

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,18 @@ var path = require('path');
 
 var OUTPUT_DIR = './dist';
 
-const fileName = 'chapter-map-component';
-const libraryName = 'ChapterMapComponent';
-
-function webpackConfig({ minimize }) {
+function webpackConfig({
+  fileName,
+  entryPath,
+  minimize,
+  externals = {},
+  libraryName = undefined
+}) {
   const publicPath = '/dist';
   const config = {
     mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
     entry: {
-      [minimize ? `${fileName}.min` : fileName]: './src/index.js'
+      [minimize ? `${fileName}.min` : fileName]: entryPath
     },
     resolve: {
       extensions: ['.js', '.jsx']
@@ -52,6 +55,7 @@ function webpackConfig({ minimize }) {
         commonjs2: 'react',
         amd: 'react'
       },
+      ...externals
     },
     devtool: 'source-map',
     plugins: [
@@ -62,44 +66,58 @@ function webpackConfig({ minimize }) {
       minimize
     }
   };
-  if (!process.env.WEBPACK_SERVE) {
-    config.externals['@gameworkers/react-simple-maps'] = {
-      root: 'ReactSimpleMaps',
-      commonjs: '@gameworkers/react-simple-maps',
-      commonjs2: '@gameworkers/react-simple-maps',
-      amd: '@gameworkers/react-simple-maps'
-    };
-    config.externals['redux'] = {
-      root: 'Redux',
-      commonjs: 'redux',
-      commonjs2: 'redux',
-      amd: 'redux'
-    };
-    config.externals['next-redux-wrapper'] = {
-      root: 'NextReduxWrapper',
-      commonjs: 'next-redux-wrapper',
-      commonjs2: 'next-redux-wrapper',
-      amd: 'next-redux-wrapper'
-    };
-    config.externals['redux-tooltip'] = {
-      root: 'ReduxTooltip',
-      commonjs: 'redux-tooltip',
-      commonjs2: 'redux-tooltip',
-      amd: 'redux-tooltip'
-    };
-    config.externals['react-redux'] = {
-      root: 'ReactRedux',
-      commonjs: 'react-redux',
-      commonjs2: 'react-redux',
-      amd: 'react-redux'
-    };
-  }
   return config;
 }
 
-module.exports = process.env.BUILD_MODE === 'unminimized'
-  ? [webpackConfig({ minimize: false })]
-  : [
-    webpackConfig({ minimize: true}),
-    webpackConfig({ minimize: false })
-  ];
+let configs = [
+  {
+    fileName: 'chapter-map-component',
+    entryPath: './src/index.js',
+    minimize: false,
+    libraryName: 'ChapterMapComponent',
+    externals: process.env.WEBPACK_SERVE
+      ? {}
+      : {
+        '@gameworkers/react-simple-maps': {
+          root: 'ReactSimpleMaps',
+          commonjs: '@gameworkers/react-simple-maps',
+          commonjs2: '@gameworkers/react-simple-maps',
+          amd: '@gameworkers/react-simple-maps'
+        },
+        'redux': {
+          root: 'Redux',
+          commonjs: 'redux',
+          commonjs2: 'redux',
+          amd: 'redux'
+        },
+        'next-redux-wrapper': {
+          root: 'NextReduxWrapper',
+          commonjs: 'next-redux-wrapper',
+          commonjs2: 'next-redux-wrapper',
+          amd: 'next-redux-wrapper'
+        },
+        'redux-tooltip': {
+          root: 'ReduxTooltip',
+          commonjs: 'redux-tooltip',
+          commonjs2: 'redux-tooltip',
+          amd: 'redux-tooltip'
+        },
+        'react-redux': {
+          root: 'ReactRedux',
+          commonjs: 'react-redux',
+          commonjs2: 'react-redux',
+          amd: 'react-redux'
+        }
+      }
+  },
+  {
+    fileName: 'demo-externals',
+    entryPath: './demo-externals.js',
+    minimize: false
+  }
+];
+if (process.env.BUILD_MODE !== 'unminimized') {
+  configs = configs.concat(configs.map(c => ({ ...c, minimize: true })));
+}
+
+module.exports = configs.map(webpackConfig);
